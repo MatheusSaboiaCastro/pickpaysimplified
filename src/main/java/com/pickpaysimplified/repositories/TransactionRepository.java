9995301ea5cf9e69dceacd8d48c730d557991c21
matheusSaboiaCastro <mnlsaboiac@gmail.com>
2025-04-11 14:34:31 -0300

    feat: create TransactionRepository

---
+++ src/main/java/com/pickpaysimplified/repositories/TransactionRepository.java
@@ -0,0 +1,14 @@
+package com.pickpaysimplified.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pickpaysimplified.domain.transaction.Transaction;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    List<Transaction> findTransactionById(String transactionId);
+
+    List<Transaction> findTransactionByUserId(Long userId);
+
+}
